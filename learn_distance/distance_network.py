fix expermints, can't clip loss too much or gradient will be zero

learn_distance/distance_network.py
@@ -25,8 +25,9 @@ class Distance_Model:
 
         if conf.loss_clip_min != None:
             clip_value_min = self.max_loss * conf.loss_clip_min
-            clip_value_max = self.max_loss * conf.loss_clip_max
-            self.loss = tf.clip_by_value(self.l2_loss, clip_value_min, clip_value_max, name='loss')
+            clip_value_max = 9999999999
+            print("clipping between {} and {}".format(clip_value_min, clip_value_max))
+            self.loss = tf.clip_by_value(self.l2_loss, clip_value_min, clip_value_max, name='loss') - clip_value_min
         else:
             self.loss = self.l2_loss
 