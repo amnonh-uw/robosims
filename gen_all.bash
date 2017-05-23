be435a801455f96e9718fe6b7ed9ca945f22e44b
Amnon Horowitz <amnonh@cs.washington.edu>
2017-05-23 14:27:11 -0700

    paralelize

--- gen_all.bash
+++ gen_all.bash
@@ -1,3 +1,19 @@
+if [ "$#" -eq 2 ]; then
+    x=$1
+    i=$2
+    dataset=`fgrep dataset $i | awk '{ print $2 }'`
+    rm -f $dataset.data
+    rm -f $dataset.idx
+    rm -f $x/$dataset.data
+    rm -f $x/$dataset.idx
+    touch $x/$dataset.data
+    touch $x/$dataset.idx
+    ln -s $x/$dataset.data .
+    ln -s $x/$dataset.idx .
+    invoke gen_train_dataset --config=$i &> gen_$dataset.out
+    exit
+fi
+
 if [ "$#" -ne 1 ]; then
     echo "gen_all.bash secondary-storage-dir"
     exit
@@ -6,10 +22,7 @@ x=$1
 
 for i in gen_dataset/*.yaml
 do
-    invoke gen_train_dataset --config=$i &>> gen_dataset.out
-    dataset=`fgrep dataset $i | awk '{ print $2 }'`
-    mv $dataset.data $x
-    mv $dataset.idx $x
-    ln -s $x/$dataset.data .
-    ln -s $x/$dataset.idx .
+    echo Generating $i in the background
+    bash gen_all.bash $x $i &
+    sleep 3
 done
