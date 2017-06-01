bc840bd0f76ecf9a877ea83adae618fc13e098a4
Amnon Horowitz <amnonh@cs.washington.edu>
2017-06-01 13:01:50 -0700

    move datasets to datasets directory

--- gen_all.bash
+++ gen_all.bash
@@ -2,15 +2,15 @@ if [ "$#" -eq 2 ]; then
     x=$1
     i=$2
     dataset=`fgrep dataset $i | awk '{ print $2 }'`
-    rm -f $dataset.data
-    rm -f $dataset.idx
+    rm -f datasets/$dataset.data
+    rm -f datasets/$dataset.idx
     rm -f $x/$dataset.data
     rm -f $x/$dataset.idx
     touch $x/$dataset.data
     touch $x/$dataset.idx
-    ln -s $x/$dataset.data .
-    ln -s $x/$dataset.idx .
-    invoke gen_train_dataset --config=$i &> gen_$dataset.out
+    ln -s $x/$dataset.data datasets/$dataset.data
+    ln -s $x/$dataset.idx datasets/$dataset.data
+    invoke gen_train_dataset --config=$i &> datasets/gen_$dataset.out
     exit
 fi
 
@@ -22,7 +22,8 @@ x=$1
 
 for i in gen_dataset/*.yaml
 do
-    echo Generating $i in the background
-    bash gen_all.bash $x $i &
+    k=`fgrep dataset $i sed -e 's/dataset: //'`
+    echo Generating $k in the background
+    bash gen_all.bash $x $k &
     sleep 3
 done
